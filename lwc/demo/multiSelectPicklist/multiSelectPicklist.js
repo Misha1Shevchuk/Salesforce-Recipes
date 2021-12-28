3ef13e55ec7c4ef1d0fa86e4f9a416b7403da4d5
Misha1Shevchuk <misha1shevchuk@gmail.com>
2021-12-28 21:37:49 +0200

    add demo for multiSelectPicklist

---
+++ lwc/demo/multiSelectPicklist/multiSelectPicklist.js
@@ -0,0 +1,15 @@
+import {LightningElement, wire} from 'lwc';
+import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import CASE_OBJECT from '@salesforce/schema/Case';
+import STATUS_FIELD from '@salesforce/schema/Case.Status';
+
+export default class MultiSelectPicklist extends LightningElement {
+
+    @wire(getObjectInfo, {objectApiName: CASE_OBJECT}) caseInfo;
+    @wire(getPicklistValues, {recordTypeId: '$caseInfo.data.defaultRecordTypeId', fieldApiName: STATUS_FIELD}) 
+    statusOptions;
+
+    handleChange(e) {
+        console.log('@@@ selected: ', JSON.stringify(e.detail.selected));
+    }
+}
