05e4c8128623c17d848844e0736d452da7ae31e1
Misha1Shevchuk <misha1shevchuk@gmail.com>
2022-01-18 12:48:44 +0200

    add a way to display cross button inside header

--- lwc/modal/modal.js
+++ lwc/modal/modal.js
@@ -5,9 +5,15 @@ export default class Modal extends LightningElement {
     @api hideHeader = false;
     @api hideFooter = false;
     @api hideCrossButton = false;
+    @api crossButtonInside = false;
 
     @api
     closeModal() {
         this.dispatchEvent(new CustomEvent('close'));
+    }
+
+    get buttonCssClass() {
+        const defaultClass = 'slds-button slds-button_icon slds-modal__close slds-button_icon-inverse';
+        return this.crossButtonInside ? `${defaultClass} close-button` : defaultClass;
     }
 }
